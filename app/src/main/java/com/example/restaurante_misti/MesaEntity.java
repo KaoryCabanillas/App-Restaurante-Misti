08c0ee31c9f19042979ca9e308c38b5bcdbd9c90
Carlos Eduardo Benjamin Rojas Salazar <erojassalazar600@gmail.com>
2024-12-04 13:10:55 -0500

    mesa create entity

--- app/src/main/java/com/example/restaurante_misti/MesaEntity.java
+++ app/src/main/java/com/example/restaurante_misti/MesaEntity.java
@@ -1,2 +1,30 @@
-package com.example.restaurante_misti;public class MesaEntity {
+package com.example.restaurante_misti;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class MesaEntity {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String n_mesa;
+
+    public MesaEntity(String n_mesa) {
+        this.n_mesa = n_mesa;
+    }
+
+    public MesaEntity(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setN_mesa(String n_mesa) {
+        this.n_mesa = n_mesa;
+    }
 }
